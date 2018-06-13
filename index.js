16eefc00931de1a40f449050582f1d94f4a42818
Андрей Алексеев <a.alekseev@liga-tyres.ru>
2018-06-13 15:52:21 +0300

    aadd

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = require('express')(),
       PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express!')
+    res.send('Hello from Express!');
 });
 
-app.post('/wall', (req, res) => '8ee1117a');
+app.post('/wall', (req, res) => {
+    res.send('8ee1117a');
+});
 
 app.listen(PORT, (err) => {
     if (err) {
