fc988f6aa8fb656371ba12669879cb93c8f7ecf6
Андрей Алексеев <a.alekseev@liga-tyres.ru>
2018-06-13 17:00:16 +0300

    11

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 //require('./app/index');
 
 const app = require('express')(),
-      PORT = process.env.PORT || 5000;
+      PORT = process.env.PORT || 5000,
+      jsonParser = require('body-parser').json();
+
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
 
-app.post('/wall', (req, res) => {
-    res.send('8ee1117a');
+// app.post('/wall', (req, res) => {
+//     res.send('8ee1117a');
+// });
+
+
+app.post('/wall', jsonParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400)
+    res.send(`This it -> ${res.type} `);
 });
 
 app.listen(PORT, (err) => {
